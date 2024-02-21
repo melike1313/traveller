d78dbe152f74c7adbac6442f2bfcf46da221f324
melike <melikenogay13@gmail.com>
2024-02-21 13:11:37 -0800

    button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,26 @@ import React,{useState} from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlane } from '@fortawesome/free-solid-svg-icons'
+import { Button } from './Button';
+import './Navbar.css';
+import './Button.css';   
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {  
+    if(window.innerWidth <= 960){
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  } ;
+
+  window.addEventListener('resize', showButton);
   return (
     <>
    
@@ -41,6 +56,7 @@ function Navbar() {
           </Link>
           </li>
 </ul>
+{button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
       </div>
     </nav>
     </>   

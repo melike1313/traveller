e9a3471635f379b7df7117d456c1cf10d9ec7cc1
melike <melikenogay13@gmail.com>
2024-02-22 21:56:07 -0800

    new

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -13,7 +13,7 @@ function HeroSection() {
     <div className='hero-container'>
         <video src="/videos/ocean.mp4" autoPlay loop muted />
       <h1>ADVENTURE AWAITS</h1>
-      <p>What are you waiting for?</p>  
+      <p className="highlight-text">What are you waiting for?</p>
       <div className='hero-btns'>   
       <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'> GET STARTED </Button>
       <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'> WATCH TRAILER WATCH TRAILER <i className='far fa-play-circle' /></Button>

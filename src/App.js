5aefd68bdd3629909724600ecc756ab20c0bf2c2
melike <melikenogay13@gmail.com>
2024-02-22 21:46:39 -0800

    new

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlane } from '@fortawesome/free-solid-svg-icons'
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './components/pages/Home';
+
 function App() {
   return (
     <Router>
-      <Navbar>
-        <Routes>
-          <Route path='/' exact />
-          <FontAwesomeIcon icon={faPlane} />
-        </Routes>
-      </Navbar>
-    </Router>  );
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
